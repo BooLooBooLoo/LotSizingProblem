333aaa5b4658aa856fbc4274a8a9cd73c7c9736f
BLSquared <92587994+BooLooBooLoo@users.noreply.github.com>
2022-11-07 18:42:14 +0100

    Modif 07/11/22

--- Projet Etudiant/SM1Method.py
+++ Projet Etudiant/SM1Method.py
@@ -18,7 +18,7 @@ def objective(t, E, dmin, dmax):
     y = m.binary_var_list(t,name='y')
     z = {k: m.binary_var_list(jVal+1, name="w_{0}".format(k)) for k in range(t)}
     w = m.integer_var_list(t,name='w')
-    I = m.integer_var_list(t+1, name='I')
+    I = m.integer_var_list(t, name='I')
 
     h=[]
     #Coût fixe de production.
@@ -29,18 +29,21 @@ def objective(t, E, dmin, dmax):
     for i in range(t):
         h.append(1)
         f.append(50)
-        c.append(dmax)
+        c.append(100)
 
     #On définit les contraintes
     for i in range(t):
         m.add_constraint(x[i]<=c[i]*y[i])
         m.add_constraint(x[i]>=0)
-        #m.add_constraint(I[0] == 0)
+        if i == 0:
+            #m.add_constraint(I[0] == 0) #Stock initial nul
+            m.add_constraint(x[i] >= w[i])
+        else :
+            m.add_constraint(I[i-1] + x[i] - I[i] >= w[i])
         m.add_constraint(z[i][0] == 0)
         m.add_constraint(m.sum(z[i][k] for k in range(1,jVal+1)) == 1)
         m.add_constraint(w[i] == m.sum(j*z[i][j-dmin] for j in range(dmin,dmax+1)))
-        m.add_constraint(I[i] >= 0)
-        m.add_constraint(I[i] + x[i] - I[i+1] >= w[i])
+        #m.add_constraint(I[i] >= 0)
         m.add_constraint(m.sum(m.sum(z[i][k]*math.log(F[k]) for k in range(jVal+1))) >= math.log(1-E))
 
     #On définit la fonction objectif
@@ -52,14 +55,14 @@ def objective(t, E, dmin, dmax):
     print("Solution value: ", m.objective_value)
     print("Solution: ")
     for i in range(t):
-        #print("x[",i,"] = ", x[i].solution_value)
-        #print("y[",i,"] = ", y[i].solution_value)
-        #print("w[",i,"] = ", w[i].solution_value)
+        print("x[",i,"] = ", x[i].solution_value)
+        print("y[",i,"] = ", y[i].solution_value)
+        print("w[",i,"] = ", w[i].solution_value)
         print("I[",i,"] = ", I[i].solution_value)
         for k in range(jVal):
             if z[i][k].solution_value == 1:
                 print("z[",i,",",k,"] = ", z[i][k].solution_value)
 
-objective(10, 0.05, 10, 200)
+objective(10, 0.05, 10, 50)
 
     
